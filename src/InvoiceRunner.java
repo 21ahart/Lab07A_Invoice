151b6a5d58170eb9d5b52d5636fb79ea4121b8db
21ahart <aidenhartranft@gmail.com>
2025-03-10 18:15:22 -0400

    edit

--- src/InvoiceRunner.java
+++ src/InvoiceRunner.java
@@ -1,6 +1,6 @@
 public class InvoiceRunner {
     public static void main(String[] args) {
         InvoiceFrame frame = new InvoiceFrame();
-        frame.displayframe();
+        frame.displayInvoice();
     }
 }
